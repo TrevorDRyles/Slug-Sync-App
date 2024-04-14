32fd11ce6f99de3b3a4749ad885341b15b49ab1d
hunttraitor <hunterrrisatratar@gmail.com>
2024-04-14 12:44:46 -0700

    Frontend Framework setup and SignIn Page Added typescript support along with postcss for mantis css. Creates a signin page to serve as an example on how to start in frontend development.

---
+++ frontend/global.d.ts
@@ -0,0 +1,2 @@
+//This file exists so that the typescript compiler doesn't complain about importing .module.css files
+declare module "*.module.css";
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-import SignIn from './components/SignIn';
+import SignIn from './components/SignIn/SignIn.tsx'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "./components/Home.jsx";
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 /**
  * App
@@ -8,12 +10,14 @@ import Home from "./components/Home.jsx";
  */
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" default element={<SignIn />} />
-        <Route path="/" default element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+  <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" default element={<SignIn />} />
+          <Route path="/" default element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
