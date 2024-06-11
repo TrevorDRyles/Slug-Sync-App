4d4c785c8b971c35cd42df3ca418d0f4608b3d45
tryles <trevorryles02@gmail.com>
2024-06-10 20:52:12 -0700

    Remove completed from goals. They shouldn't be there.

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -1,12 +1,47 @@
 -- need to all be on one line
 INSERT INTO "user"(id, data) VALUES ('1e0d7c46-2194-4a30-b8e5-1b0a7c287e80',jsonb_build_object('name', 'Hunter', 'email', 'hunter@ucsc.edu', 'password', crypt('huntertratar', 'cs'), 'img','https://i.pinimg.com/736x/32/7e/db/327edb9a15b304efc264668ada03f725.jpg', 'bio', 'Hello!'));
 INSERT INTO "user"(data) VALUES (jsonb_build_object('name', 'arelyx', 'email', 'arelyx@example.com', 'password', crypt('arelyxuser', 'cs')));
-INSERT INTO goal(id, goal) VALUES ('1e0d7c46-2194-4a30-b8e5-1b0a7c287e81', jsonb_build_object('title', 'Learn React', 'description', 'Learn React', 'recurrence', '1 day', 'author', (SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), 'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'completed', false, 'tag', 'Academics'));
-INSERT INTO goal(id, goal) VALUES ('2e0d7c46-2194-4a30-b8e5-1b0a7c287e82', jsonb_build_object('title', 'Walk a mile', 'description', 'Walk a mile every day to be healthy!', 'recurrence', '1 day', 'author', (SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), 'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'completed', false, 'tag', 'Health'));
-INSERT INTO goal(id, goal) VALUES ('3e0d7c46-2194-4a30-b8e5-1b0a7c287e83', jsonb_build_object('title', 'Eat breakfast at 9am', 'description', 'Eat breakfast at an early time to stay consistent!', 'recurrence', '1 day', 'author', (SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), 'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'completed', false, 'tag', 'Health'));
-INSERT INTO goal(id, goal) VALUES ('4e0d7c46-2194-4a30-b8e5-1b0a7c287e84', jsonb_build_object('title', 'Do your 114A homework', 'description', 'CSE 114A homework due on Tuesday at 11:59pm', 'recurrence', '1 week', 'author', (SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), 'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'completed', false, 'tag', 'Academics'));
-INSERT INTO goal(id, goal) VALUES ('5e0d7c46-2194-4a30-b8e5-1b0a7c287e85', jsonb_build_object('title', 'Get your mom a gift on Mothers Day', 'description', 'May 14th is Mothers Day, get her a gift!', 'recurrence', '1 year', 'author', (SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), 'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'completed', false, 'tag', 'Personal'));
-INSERT INTO goal(id, goal) VALUES ('6e0d7c46-2194-4a30-b8e5-1b0a7c287e86', jsonb_build_object('title', 'Go to the gym', 'description', 'Toadal fitness baybe', 'recurrence', '2 days', 'author', (SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), 'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'completed', false, 'tag', 'Health'));
+INSERT INTO goal(id, goal)
+VALUES ('1e0d7c46-2194-4a30-b8e5-1b0a7c287e81',
+        jsonb_build_object('title', 'Learn React', 'description', 'Learn React', 'recurrence', '1 day', 'author',
+                           (SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'), 'memberCount', 1,
+                           'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'tag',
+                           'Academics'));
+INSERT INTO goal(id, goal)
+VALUES ('2e0d7c46-2194-4a30-b8e5-1b0a7c287e82',
+        jsonb_build_object('title', 'Walk a mile', 'description', 'Walk a mile every day to be healthy!', 'recurrence',
+                           '1 day', 'author', (SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'),
+                           'memberCount', 1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate',
+                           '2024-05-18 20:09:57.341567', 'tag', 'Health'));
+INSERT INTO goal(id, goal)
+VALUES ('3e0d7c46-2194-4a30-b8e5-1b0a7c287e83', jsonb_build_object('title', 'Eat breakfast at 9am', 'description',
+                                                                   'Eat breakfast at an early time to stay consistent!',
+                                                                   'recurrence', '1 day', 'author',
+                                                                   (SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'),
+                                                                   'memberCount', 1, 'startdate',
+                                                                   '2024-05-18 20:09:57.341567', 'enddate',
+                                                                   '2024-05-18 20:09:57.341567', 'tag', 'Health'));
+INSERT INTO goal(id, goal)
+VALUES ('4e0d7c46-2194-4a30-b8e5-1b0a7c287e84', jsonb_build_object('title', 'Do your 114A homework', 'description',
+                                                                   'CSE 114A homework due on Tuesday at 11:59pm',
+                                                                   'recurrence', '1 week', 'author',
+                                                                   (SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'),
+                                                                   'memberCount', 1, 'startdate',
+                                                                   '2024-05-18 20:09:57.341567', 'enddate',
+                                                                   '2024-05-18 20:09:57.341567', 'tag', 'Academics'));
+INSERT INTO goal(id, goal)
+VALUES ('5e0d7c46-2194-4a30-b8e5-1b0a7c287e85',
+        jsonb_build_object('title', 'Get your mom a gift on Mothers Day', 'description',
+                           'May 14th is Mothers Day, get her a gift!', 'recurrence', '1 year', 'author',
+                           (SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'), 'memberCount', 1,
+                           'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'tag',
+                           'Personal'));
+INSERT INTO goal(id, goal)
+VALUES ('6e0d7c46-2194-4a30-b8e5-1b0a7c287e86',
+        jsonb_build_object('title', 'Go to the gym', 'description', 'Toadal fitness baybe', 'recurrence', '2 days',
+                           'author', (SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'), 'memberCount',
+                           1, 'startdate', '2024-05-18 20:09:57.341567', 'enddate', '2024-05-18 20:09:57.341567', 'tag',
+                           'Health'));
 INSERT INTO user_goal VALUES ((SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'), (SELECT id FROM goal WHERE goal ->> 'title' = 'Walk a mile'), CURRENT_TIMESTAMP, 1);
 INSERT INTO user_goal VALUES ((SELECT id FROM "user" WHERE data ->> 'email' = 'hunter@ucsc.edu'), (SELECT id FROM goal WHERE goal ->> 'title' = 'Eat breakfast at 9am'), '2024-05-18 20:09:57.341567'::timestamp, 16);
 INSERT INTO user_goal VALUES ((SELECT id FROM "user" WHERE data->>'email' = 'hunter@ucsc.edu'), (SELECT id FROM goal WHERE goal->>'title' = 'Do your 114A homework'), '2024-05-12 20:09:57.341567'::timestamp, 3);
