a178815718413aaf684308f7a1740b63a2706f9c
tryles <trevorryles02@gmail.com>
2024-06-11 11:48:01 -0700

    Dashboard tests

--- e2e/tests/dashboard.test.js
+++ e2e/tests/dashboard.test.js
@@ -20,7 +20,7 @@ let page;
 beforeAll(() => {
   backend = http.createServer(app);
   backend.listen(3010, () => {
-    console.log('Backend Running at http://localhost:3010');
+    // console.log('Backend Running at http://localhost:3010');
   });
   frontend = http.createServer(
     express()
@@ -32,7 +32,7 @@ beforeAll(() => {
       }),
   );
   frontend.listen(3000, () => {
-    console.log('Frontend Running at http://localhost:3000');
+    // console.log('Frontend Running at http://localhost:3000');
   });
 });
 
@@ -51,11 +51,11 @@ afterAll((done) => {
  */
 beforeEach(async () => {
   browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   page = await browser.newPage();
   page.on('console', (msg) => {
-    console.log('Browser log:', msg.text());
+    // console.log('Browser log:', msg.text());
   });
   await page.setViewport({
     width: 1080,
@@ -71,97 +71,101 @@ afterEach(async () => {
   await browser.close();
 });
 
-const signUp = async(page, name, email, password) => {
-  await page.goto('http://localhost:3000/login')
-  await page.waitForFunction(
-    'document.querySelector("body").innerText.includes("Login")',
-  );
-  await page.click('[aria-label="Switch Signin"')
-  await page.waitForFunction(
-    'document.querySelector("body").innerText.includes("Register")',
-  );
-  const nameInput = await page.$('aria/Name Input')
-  await nameInput.type(name);
-  const emailInput = await page.$('aria/Email Input')
-  await emailInput.type(email);
-  const passwordInput = await page.$('aria/Password Input')
-  await passwordInput.type(password)
-  await page.click('aria/Submit Signin Button');
-  page.on('dialog', async dialog => {
-    await dialog.accept()
-  })
-}
+// const signUp = async (page, name, email, password) => {
+//   await page.goto('http://localhost:3000/login');
+//   await page.waitForFunction(
+//     'document.querySelector("body").innerText.includes("Login")',
+//   );
+//   await page.click('[aria-label="Switch Signin"');
+//   await page.waitForFunction(
+//     'document.querySelector("body").innerText.includes("Register")',
+//   );
+//   const nameInput = await page.$('aria/Name Input');
+//   await nameInput.type(name);
+//   const emailInput = await page.$('aria/Email Input');
+//   await emailInput.type(email);
+//   const passwordInput = await page.$('aria/Password Input');
+//   await passwordInput.type(password);
+//   await page.click('aria/Submit Signin Button');
+//   page.on('dialog', async (dialog) => {
+//     await dialog.accept();
+//   });
+// };
 
-const logIn = async(page, email, password) => {
-  await page.goto('http://localhost:3000/login')
-  const emailInput = await page.$('aria/Email Input')
+const logIn = async (page, email, password) => {
+  await page.goto('http://localhost:3000/login');
+  const emailInput = await page.$('aria/Email Input');
   await emailInput.type(email);
-  const passwordInput = await page.$('aria/Password Input')
-  await passwordInput.type(password)
-  await page.click('aria/Submit Signin Button')
+  const passwordInput = await page.$('aria/Password Input');
+  await passwordInput.type(password);
+  await page.click('aria/Submit Signin Button');
   await page.waitForFunction(
     'document.querySelector("body").innerText.includes("Complete")',
-  )
-}
+  );
+};
 
-test('Adds a new goal', async() => {
-  await logIn(page, "e2etester@gmail.com", "password");
-  await page.hover('aria/Goals dropdown')
-  await page.click('aria/Create Goal Button')
+test('Adds a new goal', async () => {
+  await logIn(page, 'e2etester@gmail.com', 'password');
+  await page.hover('aria/Goals dropdown');
+  await page.click('aria/Create Goal Button');
   await page.waitForFunction(
     'document.querySelector("body").innerText.includes("Create Goal")',
-  )
-  const titleInput = await page.$('aria/Title Input')
-  await titleInput.type("Learn React")
-  const descriptionInput = await page.$('aria/Description Input')
-  await descriptionInput.type('This is a goal to learn react for 115A!')
-  await page.click('aria/Tag Input')
+  );
+  const titleInput = await page.$('aria/Title Input');
+  await titleInput.type('Learn React');
+  const descriptionInput = await page.$('aria/Description Input');
+  await descriptionInput.type('This is a goal to learn react for 115A!');
+  await page.click('aria/Tag Input');
   await page.evaluate(() => {
-    const element = document.querySelector('[value="Productivity"]')
+    const element = document.querySelector('[value="Productivity"]');
     if (element) {
       element.click();
     }
-  })
-  await page.waitForSelector('aria/Select start date')
+  });
+  await page.waitForSelector('aria/Select start date');
   await page.click('aria/Select start date');
   await page.waitForSelector('aria/11 June 2024');
-  await page.click('aria/11 June 2024')
+  await page.click('aria/11 June 2024');
   await page.waitForSelector('aria/Select end date');
-  await page.click('aria/Select end date')
+  await page.click('aria/Select end date');
   await page.waitForSelector('aria/12 June 2024');
-  await page.click('aria/12 June 2024')
+  await page.click('aria/12 June 2024');
   await page.waitForSelector('aria/Submit New Goal');
-  await page.click('aria/Submit New Goal')
+  await page.click('aria/Submit New Goal');
   await page.waitForFunction(
     'document.querySelector("body").innerText.includes("Learn React")',
-  )
-})
+  );
+});
 
-test('Clicks complete goal', async() => {
-  await logIn(page, "e2etester@gmail.com", "password")
+test('Clicks complete goal', async () => {
+  await logIn(page, 'e2etester@gmail.com', 'password');
   await page.waitForFunction(
     'document.querySelector("body").innerText.includes("0")',
-  )
+  );
+  await page.waitForSelector('aria/Goal not completed');
   await page.click('aria/Goal not completed');
   await page.waitForSelector('aria/Goal completed');
   await page.waitForFunction(
     'document.querySelector("body").innerText.includes("1")',
-  )
+  );
 });
 
-test('Deletes goal cleanup', async() => {
-  await logIn(page, "e2etester@gmail.com", "password")
-  await page.hover('aria/Goals dropdown')
+test('Deletes goal cleanup', async () => {
+  await logIn(page, 'e2etester@gmail.com', 'password');
+  await page.hover('aria/Goals dropdown');
+  await page.waitForSelector('aria/View Goal Button');
   await page.click('aria/View Goal Button');
-  await page.waitForSelector('aria/Search bar input')
-  const searchInput = await page.$('aria/Search bar input')
-  searchInput.type("Learn React")
-  await page.waitForSelector('aria/Goal Title Text')
-  await page.click('aria/Goal Title Text')
-  await page.waitForSelector('aria/Delete Goal')
-  await page.click('aria/Delete Goal')
-  await page.click('aria/Delete Goal')
+  await page.waitForSelector('aria/Search bar input');
+  const searchInput = await page.$('aria/Search bar input');
+  await searchInput.type('Learn React');
+  await page.waitForSelector('aria/Goal Title Text');
+  await page.click('aria/Goal Title Text');
+  await page.waitForSelector('aria/Delete Goal');
+  await page.click('aria/Delete Goal');
+  // Re-select the 'Delete Goal' button before clicking it again
+  const deleteGoalButton = await page.$('aria/Delete Goal');
+  await deleteGoalButton.click();
   await page.waitForFunction(
     'document.querySelector("body").innerText.includes("Goals")',
-  )
-})
+  );
+});
